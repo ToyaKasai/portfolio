8f42fdda5a44bee6aee8241533b11115e2fcb659
ToyaKasai <kasaitoya@team-lab.com>
2022-06-24 17:28:40 +0900

    feat: add MainVisual component

---
+++ components/pages/MainVisual.tsx
@@ -0,0 +1,19 @@
+import { FC } from 'react';
+
+const MainVisual: FC = () => {
+  return (
+    <div className="flex h-screen w-full items-center justify-start">
+      <p className="pl-16 text-9xl font-black leading-tight">
+        FRONTEND
+        <br />
+        ENGINEER
+        <br />
+        TOYA KASAI
+        <br />
+        PORTFOLIO 2022
+      </p>
+    </div>
+  );
+};
+
+export default MainVisual;
