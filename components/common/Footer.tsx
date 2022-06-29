12e0d57c8daee4f7bb40f775816ff6a18d99b36b
ToyaKasai <kasaitoya@team-lab.com>
2022-06-29 23:08:57 +0900

    feat: add Footer

---
+++ components/common/Footer.tsx
@@ -0,0 +1,11 @@
+import { FC } from 'react';
+
+const Footer: FC = () => {
+  return (
+    <footer className="flex h-16 w-full items-center justify-center">
+      &copy;Toya Kasai
+    </footer>
+  );
+};
+
+export default Footer;
